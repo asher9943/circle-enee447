fa15fa370a9e94293bacfa69f777334dbeb5426e
Asher Anand <asherganand@gmail.com>
2024-02-26 15:24:30 -0500

    put in yield code, now we need mess with exceptionstub

--- lib/sched/scheduler.cpp
+++ lib/sched/scheduler.cpp
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2015-2021  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -28,23 +28,23 @@ static const char FromScheduler[] = "sched";
 
 CScheduler *CScheduler::s_pThis = 0;
 
-CScheduler::CScheduler (void)
-:	m_nTasks (0),
-	m_pCurrent (0),
-	m_nCurrent (0),
-	m_pTaskSwitchHandler (0),
-	m_pTaskTerminationHandler (0),
-	m_iSuspendNewTasks (0)
+CScheduler::CScheduler(void)
+	: m_nTasks(0),
+	  m_pCurrent(0),
+	  m_nCurrent(0),
+	  m_pTaskSwitchHandler(0),
+	  m_pTaskTerminationHandler(0),
+	  m_iSuspendNewTasks(0)
 {
-	assert (s_pThis == 0);
+	assert(s_pThis == 0);
 	s_pThis = this;
 
-	m_pCurrent = new CTask (0);		// main task currently running
-	assert (m_pCurrent != 0);
-	m_pCurrent->SetName ("main");
+	m_pCurrent = new CTask(0); // main task currently running
+	assert(m_pCurrent != 0);
+	m_pCurrent->SetName("main");
 }
 
-CScheduler::~CScheduler (void)
+CScheduler::~CScheduler(void)
 {
 	m_pTaskSwitchHandler = 0;
 	m_pTaskTerminationHandler = 0;
@@ -52,7 +52,7 @@ CScheduler::~CScheduler (void)
 	s_pThis = 0;
 }
 
-void CScheduler::Yield (void)
+void CScheduler::Yield(void)
 {
 	// Cooperative multitasking is disabled for project 2 because it requires
 	//   extra effort to make it compatible with preemptive multitasking
@@ -70,7 +70,7 @@ void CScheduler::Yield (void)
 	{
 		return;
 	}
-	
+
 	TTaskRegisters *pOldRegs = m_pCurrent->GetRegs ();
 	m_pCurrent = pNext;
 	TTaskRegisters *pNewRegs = m_pCurrent->GetRegs ();
@@ -86,60 +86,59 @@ void CScheduler::Yield (void)
 	*/
 }
 
-void CScheduler::Sleep (unsigned nSeconds)
+void CScheduler::Sleep(unsigned nSeconds)
 {
 	// be sure the clock does not run over taken as signed int
-	const unsigned nSleepMax = 1800;	// normally 2147 but to be sure
+	const unsigned nSleepMax = 1800; // normally 2147 but to be sure
 	while (nSeconds > nSleepMax)
 	{
-		usSleep (nSleepMax * 1000000);
+		usSleep(nSleepMax * 1000000);
 
 		nSeconds -= nSleepMax;
 	}
 
-	usSleep (nSeconds * 1000000);
+	usSleep(nSeconds * 1000000);
 }
 
-void CScheduler::MsSleep (unsigned nMilliSeconds)
+void CScheduler::MsSleep(unsigned nMilliSeconds)
 {
 	if (nMilliSeconds > 0)
 	{
-		usSleep (nMilliSeconds * 1000);
+		usSleep(nMilliSeconds * 1000);
 	}
 }
 
-void CScheduler::usSleep (unsigned nMicroSeconds)
+void CScheduler::usSleep(unsigned nMicroSeconds)
 {
 	if (nMicroSeconds > 0)
 	{
 		unsigned nTicks = nMicroSeconds * (CLOCKHZ / 1000000);
 
-		unsigned nStartTicks = CTimer::Get ()->GetClockTicks ();
+		unsigned nStartTicks = CTimer::Get()->GetClockTicks();
 
-		assert (m_pCurrent != 0);
-		assert (m_pCurrent->GetState () == TaskStateReady);
-		m_pCurrent->SetWakeTicks (nStartTicks + nTicks);
-		m_pCurrent->SetState (TaskStateSleeping);
+		assert(m_pCurrent != 0);
+		assert(m_pCurrent->GetState() == TaskStateReady);
+		m_pCurrent->SetWakeTicks(nStartTicks + nTicks);
+		m_pCurrent->SetState(TaskStateSleeping);
 
-		Yield ();
+		Yield();
 	}
 }
 
-CTask *CScheduler::GetCurrentTask (void)
+CTask *CScheduler::GetCurrentTask(void)
 {
 	return m_pCurrent;
 }
 
-CTask *CScheduler::GetTask (const char *pTaskName)
+CTask *CScheduler::GetTask(const char *pTaskName)
 {
-	assert (pTaskName != 0);
+	assert(pTaskName != 0);
 
 	for (unsigned i = 0; i < m_nTasks; i++)
 	{
 		CTask *pTask = m_pTask[i];
 
-		if (   pTask != 0
-		    && strcmp (pTask->GetName (), pTaskName) == 0)
+		if (pTask != 0 && strcmp(pTask->GetName(), pTaskName) == 0)
 		{
 			return pTask;
 		}
@@ -148,7 +147,7 @@ CTask *CScheduler::GetTask (const char *pTaskName)
 	return 0;
 }
 
-boolean CScheduler::IsValidTask (CTask *pTask)
+boolean CScheduler::IsValidTask(CTask *pTask)
 {
 	unsigned i;
 	for (i = 0; i < m_nTasks; i++)
@@ -160,26 +159,26 @@ boolean CScheduler::IsValidTask (CTask *pTask)
 	return FALSE;
 }
 
-void CScheduler::RegisterTaskSwitchHandler (TSchedulerTaskHandler *pHandler)
+void CScheduler::RegisterTaskSwitchHandler(TSchedulerTaskHandler *pHandler)
 {
-	assert (m_pTaskSwitchHandler == 0);
+	assert(m_pTaskSwitchHandler == 0);
 	m_pTaskSwitchHandler = pHandler;
-	assert (m_pTaskSwitchHandler != 0);
+	assert(m_pTaskSwitchHandler != 0);
 }
 
-void CScheduler::RegisterTaskTerminationHandler (TSchedulerTaskHandler *pHandler)
+void CScheduler::RegisterTaskTerminationHandler(TSchedulerTaskHandler *pHandler)
 {
-	assert (m_pTaskTerminationHandler == 0);
+	assert(m_pTaskTerminationHandler == 0);
 	m_pTaskTerminationHandler = pHandler;
-	assert (m_pTaskTerminationHandler != 0);
+	assert(m_pTaskTerminationHandler != 0);
 }
 
-void CScheduler::SuspendNewTasks (void)
+void CScheduler::SuspendNewTasks(void)
 {
 	m_iSuspendNewTasks++;
 }
 
-void CScheduler::ResumeNewTasks (void)
+void CScheduler::ResumeNewTasks(void)
 {
 	assert(m_iSuspendNewTasks > 0);
 	m_iSuspendNewTasks--;
@@ -194,16 +193,15 @@ void CScheduler::ResumeNewTasks (void)
 				m_pTask[i]->Start();
 			}
 		}
-
 	}
 }
 
-void CScheduler::ListTasks (CDevice *pTarget)
+void CScheduler::ListTasks(CDevice *pTarget)
 {
-	assert (pTarget != 0);
+	assert(pTarget != 0);
 
 	static const char Header[] = "#  ADDR     STAT  FL NAME\n";
-	pTarget->Write (Header, sizeof Header-1);
+	pTarget->Write(Header, sizeof Header - 1);
 
 	for (unsigned i = 0; i < m_nTasks; i++)
 	{
@@ -213,28 +211,28 @@ void CScheduler::ListTasks (CDevice *pTarget)
 			continue;
 		}
 
-		TTaskState State = pTask->GetState ();
-		assert (State < TaskStateUnknown);
+		TTaskState State = pTask->GetState();
+		assert(State < TaskStateUnknown);
 
 		// must match CTask::TTaskState
 		static const char *StateNames[] =
 			{"new", "ready", "block", "block", "sleep", "term"};
 
 		CString Line;
-		Line.Format ("%02u %08lX %-5s %c%c %s\n",
-			     i, (uintptr) pTask,
-			     pTask == m_pCurrent ? "run" : StateNames[State],
-			     pTask->IsSuspended () ? 'S' : ' ',
-			     State == TaskStateBlockedWithTimeout ? 'T' : ' ',
-			     pTask->GetName ());
+		Line.Format("%02u %08lX %-5s %c%c %s\n",
+					i, (uintptr)pTask,
+					pTask == m_pCurrent ? "run" : StateNames[State],
+					pTask->IsSuspended() ? 'S' : ' ',
+					State == TaskStateBlockedWithTimeout ? 'T' : ' ',
+					pTask->GetName());
 
-		pTarget->Write (Line, Line.GetLength ());
+		pTarget->Write(Line, Line.GetLength());
 	}
 }
 
-void CScheduler::AddTask (CTask *pTask)
+void CScheduler::AddTask(CTask *pTask)
 {
-	assert (pTask != 0);
+	assert(pTask != 0);
 
 	if (m_iSuspendNewTasks)
 	{
@@ -254,13 +252,13 @@ void CScheduler::AddTask (CTask *pTask)
 
 	if (m_nTasks >= MAX_TASKS)
 	{
-		CLogger::Get ()->Write (FromScheduler, LogPanic, "System limit of tasks exceeded");
+		CLogger::Get()->Write(FromScheduler, LogPanic, "System limit of tasks exceeded");
 	}
 
 	m_pTask[m_nTasks++] = pTask;
 }
 
-void CScheduler::RemoveTask (CTask *pTask)
+void CScheduler::RemoveTask(CTask *pTask)
 {
 	for (unsigned i = 0; i < m_nTasks; i++)
 	{
@@ -268,7 +266,7 @@ void CScheduler::RemoveTask (CTask *pTask)
 		{
 			m_pTask[i] = 0;
 
-			if (i == m_nTasks-1)
+			if (i == m_nTasks - 1)
 			{
 				m_nTasks--;
 			}
@@ -277,17 +275,17 @@ void CScheduler::RemoveTask (CTask *pTask)
 		}
 	}
 
-	assert (0);
+	assert(0);
 }
 
-boolean CScheduler::BlockTask (CTask **ppWaitListHead, unsigned nMicroSeconds)
+boolean CScheduler::BlockTask(CTask **ppWaitListHead, unsigned nMicroSeconds)
 {
-	assert (ppWaitListHead != 0);
-	assert (m_pCurrent->m_pWaitListNext == 0);
-	assert (m_pCurrent != 0);
-	assert (m_pCurrent->GetState () == TaskStateReady);
+	assert(ppWaitListHead != 0);
+	assert(m_pCurrent->m_pWaitListNext == 0);
+	assert(m_pCurrent != 0);
+	assert(m_pCurrent->GetState() == TaskStateReady);
 
-	m_SpinLock.Acquire ();
+	m_SpinLock.Acquire();
 
 	// Add current task to waiting task list
 	m_pCurrent->m_pWaitListNext = *ppWaitListHead;
@@ -295,28 +293,28 @@ boolean CScheduler::BlockTask (CTask **ppWaitListHead, unsigned nMicroSeconds)
 
 	if (nMicroSeconds == 0)
 	{
-		m_pCurrent->SetState (TaskStateBlocked);
+		m_pCurrent->SetState(TaskStateBlocked);
 	}
 	else
 	{
 		unsigned nTicks = nMicroSeconds * (CLOCKHZ / 1000000);
-		unsigned nStartTicks = CTimer::Get ()->GetClockTicks ();
+		unsigned nStartTicks = CTimer::Get()->GetClockTicks();
 
-		m_pCurrent->SetWakeTicks (nStartTicks + nTicks);
-		m_pCurrent->SetState (TaskStateBlockedWithTimeout);
+		m_pCurrent->SetWakeTicks(nStartTicks + nTicks);
+		m_pCurrent->SetState(TaskStateBlockedWithTimeout);
 	}
-	
-	m_SpinLock.Release ();
 
-	Yield ();
+	m_SpinLock.Release();
 
-	m_SpinLock.Acquire ();
+	Yield();
+
+	m_SpinLock.Acquire();
 
 	// Remove this task from the wait list in case was woken by timeout and
-	// not by the event signalling (in which case the list will already be 
+	// not by the event signalling (in which case the list will already be
 	// cleared and the following is a no-op)
-	CTask* pPrev = 0;
-	CTask* p = *ppWaitListHead;
+	CTask *pPrev = 0;
+	CTask *p = *ppWaitListHead;
 	while (p)
 	{
 		if (p == m_pCurrent)
@@ -331,17 +329,17 @@ boolean CScheduler::BlockTask (CTask **ppWaitListHead, unsigned nMicroSeconds)
 	}
 	m_pCurrent->m_pWaitListNext = nullptr;
 
-	m_SpinLock.Release ();
+	m_SpinLock.Release();
 
 	// GetWakeTicks Will be zero if timeout expired, non-zero if event signalled
-	return m_pCurrent->GetWakeTicks() == 0;		
+	return m_pCurrent->GetWakeTicks() == 0;
 }
 
-void CScheduler::WakeTasks (CTask **ppWaitListHead)
+void CScheduler::WakeTasks(CTask **ppWaitListHead)
 {
-	assert (ppWaitListHead != 0);
+	assert(ppWaitListHead != 0);
 
-	m_SpinLock.Acquire ();
+	m_SpinLock.Acquire();
 
 	CTask *pTask = *ppWaitListHead;
 	*ppWaitListHead = 0;
@@ -349,33 +347,30 @@ void CScheduler::WakeTasks (CTask **ppWaitListHead)
 	while (pTask)
 	{
 #ifdef NDEBUG
-		if (   pTask == 0
-		    ||    (pTask->GetState () != TaskStateBlocked
-		       && pTask->GetState () != TaskStateBlockedWithTimeout))
+		if (pTask == 0 || (pTask->GetState() != TaskStateBlocked && pTask->GetState() != TaskStateBlockedWithTimeout))
 		{
-			CLogger::Get ()->Write (FromScheduler, LogPanic, "Tried to wake non-blocked task");
+			CLogger::Get()->Write(FromScheduler, LogPanic, "Tried to wake non-blocked task");
 		}
 #else
-		assert (pTask != 0);
-		assert (   pTask->GetState () == TaskStateBlocked
-		        || pTask->GetState () == TaskStateBlockedWithTimeout);
+		assert(pTask != 0);
+		assert(pTask->GetState() == TaskStateBlocked || pTask->GetState() == TaskStateBlockedWithTimeout);
 #endif
 
-		pTask->SetState (TaskStateReady);
+		pTask->SetState(TaskStateReady);
 
-		CTask* pNext = pTask->m_pWaitListNext;
+		CTask *pNext = pTask->m_pWaitListNext;
 		pTask->m_pWaitListNext = 0;
 		pTask = pNext;
 	}
 
-	m_SpinLock.Release ();
+	m_SpinLock.Release();
 }
 
-unsigned CScheduler::GetNextTask (void)
+unsigned CScheduler::GetNextTask(void)
 {
 	unsigned nTask = m_nCurrent < MAX_TASKS ? m_nCurrent : 0;
 
-	unsigned nTicks = CTimer::Get ()->GetClockTicks ();
+	unsigned nTicks = CTimer::Get()->GetClockTicks();
 
 	for (unsigned i = 1; i <= m_nTasks; i++)
 	{
@@ -390,12 +385,12 @@ unsigned CScheduler::GetNextTask (void)
 			continue;
 		}
 
-		if (pTask->IsSuspended ())
+		if (pTask->IsSuspended())
 		{
 			continue;
 		}
 
-		switch (pTask->GetState ())
+		switch (pTask->GetState())
 		{
 		case TaskStateReady:
 			return nTask;
@@ -405,34 +400,33 @@ unsigned CScheduler::GetNextTask (void)
 			continue;
 
 		case TaskStateBlockedWithTimeout:
-			if ((int) (pTask->GetWakeTicks () - nTicks) > 0)
+			if ((int)(pTask->GetWakeTicks() - nTicks) > 0)
 			{
 				continue;
 			}
-			pTask->SetState (TaskStateReady);
-			pTask->SetWakeTicks(0);		// Use as flag that timeout expired
+			pTask->SetState(TaskStateReady);
+			pTask->SetWakeTicks(0); // Use as flag that timeout expired
 			return nTask;
 
-
 		case TaskStateSleeping:
-			if ((int) (pTask->GetWakeTicks () - nTicks) > 0)
+			if ((int)(pTask->GetWakeTicks() - nTicks) > 0)
 			{
 				continue;
 			}
-			pTask->SetState (TaskStateReady);
+			pTask->SetState(TaskStateReady);
 			return nTask;
 
 		case TaskStateTerminated:
 			if (m_pTaskTerminationHandler != 0)
 			{
-				(*m_pTaskTerminationHandler) (pTask);
+				(*m_pTaskTerminationHandler)(pTask);
 			}
-			RemoveTask (pTask);
+			RemoveTask(pTask);
 			delete pTask;
 			return MAX_TASKS;
 
 		default:
-			assert (0);
+			assert(0);
 			break;
 		}
 	}
@@ -440,49 +434,52 @@ unsigned CScheduler::GetNextTask (void)
 	return MAX_TASKS;
 }
 
-CScheduler *CScheduler::Get (void)
+CScheduler *CScheduler::Get(void)
 {
-	assert (s_pThis != 0);
+	assert(s_pThis != 0);
 	return s_pThis;
 }
 
 int should_contextswith_on_irq_return;
 unsigned timertick_of_last_contextswitch;
 
-void a_simple_timer_interrupt_handler(void) {
+void a_simple_timer_interrupt_handler(void)
+{
 	unsigned current_timertick = CTimer::Get()->GetTicks();
-	if (current_timertick - timertick_of_last_contextswitch >=  HZ) {
+	if (current_timertick - timertick_of_last_contextswitch >= HZ)
+	{
 		// If the interrupted task has used up its CPU time slice,
 		//   do a context switch.
 		should_contextswith_on_irq_return = 1;
 		timertick_of_last_contextswitch = current_timertick;
 	}
-	else {
+	else
+	{
 		should_contextswith_on_irq_return = 0;
 	}
 }
 
-
-void CScheduler::EnablePreemptiveMultitasking() {
-	#if RASPPI != 1
-	#error "This implementation of preemptive multitasking only works for RASPPI==1!"
-	#endif
-	#if AARCH != 32
-	#error "This implementation of preemptive multitasking only works for 32-bit ARM!"
-	#endif
-	#ifdef ARM_ALLOW_MULTI_CORE
-	#error "This implementation of preemptive multitasking only works for single-core CPU!"
-	#endif
+void CScheduler::EnablePreemptiveMultitasking()
+{
+#if RASPPI != 1
+#error "This implementation of preemptive multitasking only works for RASPPI==1!"
+#endif
+#if AARCH != 32
+#error "This implementation of preemptive multitasking only works for 32-bit ARM!"
+#endif
+#ifdef ARM_ALLOW_MULTI_CORE
+#error "This implementation of preemptive multitasking only works for single-core CPU!"
+#endif
 
 	should_contextswith_on_irq_return = 0;
 	timertick_of_last_contextswitch = CTimer::Get()->GetTicks();
 	CTimer::Get()->RegisterPeriodicHandler(a_simple_timer_interrupt_handler);
 }
 
-void ContextSwitchOnIrqReturn_by_modifyingTaskContextSavedByIrqStub(TTaskRegisters* regs_saved_by_irq_stub) {
+void ContextSwitchOnIrqReturn_by_modifyingTaskContextSavedByIrqStub(TTaskRegisters *regs_saved_by_irq_stub)
+{
 	should_contextswith_on_irq_return = 0;
-	CScheduler* scheduler = CScheduler::Get();
-	CTask *pNext = scheduler->m_pCurrent;
+	CScheduler *scheduler = CScheduler::Get();
 
 	// TODO: You should borrow all codes form Yield but make the following changes:
 	//   1. Use the variable `scheduler` above to fix any compilation errors.
@@ -491,6 +488,38 @@ void ContextSwitchOnIrqReturn_by_modifyingTaskContextSavedByIrqStub(TTaskRegiste
 	//     fully understood how IRQStub performs context switch), then write code here to
 	//     make the function do exactly what it is supposed to do.
 
-	CLogger::Get ()->Write (FromScheduler, LogDebug, "Current task is task %s, will switch to task %s.\n", scheduler->m_pCurrent->GetName(), pNext->GetName());
+	CLogger::Get()->Write(FromScheduler, LogDebug, "Current task is task %s, will switch to task %s.\n", scheduler->m_pCurrent->GetName(), pNext->GetName());
 
+	while ((scheduler->m_nCurrent = scheduler->GetNextTask()) == MAX_TASKS) // no task is ready
+	{
+		assert(scheduler->m_nTasks > 0);
+	}
+
+	assert(scheduler->m_nCurrent < MAX_TASKS);
+
+	CTask *pNext = scheduler->m_pTask[scheduler->m_nCurrent];
+
+	assert(pNext != 0);
+
+	if (scheduler->m_pCurrent == pNext)
+	{
+		return;
+	}
+
+	TTaskRegisters *pOldRegs = scheduler->m_pCurrent->GetRegs();
+
+	scheduler->m_pCurrent = pNext;
+
+	TTaskRegisters *pNewRegs = scheduler->m_pCurrent->GetRegs();
+
+	if (scheduler->m_pTaskSwitchHandler != 0)
+	{
+		(*(scheduler->m_pTaskSwitchHandler))(scheduler->m_pCurrent);
+	}
+
+	assert(pOldRegs != 0);
+
+	assert(pNewRegs != 0);
+
+	TaskSwitch(pOldRegs, pNewRegs);
 }
