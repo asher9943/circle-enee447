c4b7580aedec775c20ad5616a764797bf67ea0ce
Rick Strucko <richardcstrucko@gmail.com>
2024-04-30 13:11:16 -0400

    Implement switch for system calls, gettime syscall

--- lib/syscallhandler.cpp
+++ lib/syscallhandler.cpp
@@ -2,12 +2,39 @@
 #include <circle/logger.h>
 #include <circle/sched/scheduler.h>
 #include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+int system_time = 0;
 
 int SyscallHandler(int arg1, int arg2, int arg3, int arg4) {
 	register long syscall_no asm ("r7");
-	// CLogger::Get ()->Write ("SyscallHandler", LogDebug, "syscall_no=0x%x.", syscall_no);
+	LOGMODULE("SyscallHandler");
+	int val_to_ret = 0;
 
-	// TODO: Handle system calls based on syscall_no.
+	switch(syscall_no) {
+		case 0: {	// gettime system call
+			val_to_ret = system_time;
+			system_time++;
+			break;
+		}
+		case 1: {	// get_task_name system call
 
-	return 0;
+		}
+		case 2: {	// print system call (note arg1 contains the pointer to the character string to print)
+			
+		}
+		case 3: {	// sleep system call (note arg1 contains the integer representing the seconds to sleep)
+
+		}
+		case 4: {	// exit system call
+			
+		}
+		default: {
+			LOGERR("System call number not recognized");
+			val_to_ret = -1;
+		}
+	}
+
+	return val_to_ret;
 }
